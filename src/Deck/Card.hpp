2232dd820b3a5e9471614770ef47e9d32cf38cf2
kretkiew <lukasz.kretkiewicz@nokia.com>
2022-04-02 16:41:50 +0200

    Created test isDeckGeneratedWith52ExclusiveCards and Deck and Pile classes

--- src/Deck/Card.hpp
+++ src/Deck/Card.hpp
@@ -17,6 +17,14 @@ enum class Figures {
   Ace
 };
 
+constexpr std::initializer_list<Figures> figures_all = {
+    Figures::Two,   Figures::Three, Figures::Four, Figures::Five, Figures::Six,
+    Figures::Seven, Figures::Eigth, Figures::Nine, Figures::Ten,  Figures::Jack,
+    Figures::Queen, Figures::King,  Figures::Ace};
+
+constexpr std::initializer_list<Color> colors_all = {
+    Color::Clubs, Color::Spades, Color::Diamonds, Color::Hearts};
+
 struct Card {
   Color color;
   Figures figure;
--- src/Deck/Deck.hpp
+++ src/Deck/Deck.hpp
@@ -6,7 +6,11 @@ class Deck
     public:
     Deck()
     {
-
+        for(auto figure : figures_all)
+        {
+            for(auto color : colors_all)
+            cards.push_back(Card{color, figure});
+        }
     }
     auto getCards() const
     {
